9e78b235efe12d35e0b47175c0c1e1fa8e36ff5e
chefboyrdeuce <jpldeuce@gmail.com>
2019-09-07 23:41:38 -0400

    fetching data and rendering photo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React,  { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [photo, setPhoto] = useState("")
+  const [desc, setDesc] = useState("")
+    useEffect(() => {
+        axios.get('https://api.nasa.gov/planetary/apod?api_key=8fLI3JEAa1VEwQbWynlRRpoQwa0lHCl3TcuI5Z6d&date=2019-09-06')
+        .then((response) => {
+            console.log(response.data)
+            setPhoto(response.data.url)
+            setDesc(response.data.explination)
+        })
+        .catch((error) => {
+            console.log('Network request was unsuccesful')
+            console.log(error)
+        })
+  
+      
+    });
+  
+  
+
   return (
     <div className="App">
+      <img src = {photo} />  
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+       {desc } 🚀!
       </p>
     </div>
   );
 }
 
 export default App;
+
+
